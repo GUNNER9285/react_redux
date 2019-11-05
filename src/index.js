f35f8cf201493647f7a0a5253661010e6cf59c8d
Kran Thaweesith <kran.tha@truedigital.com>
2019-11-05 09:39:06 +0700

    Redux : action & reducer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,81 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+//
+// ReactDOM.render(<App />, document.getElementById('root'));
+//
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+// //npm install -g redux --save
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {createStore, combineReducers} from "redux";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const initialState = {
+    salary: 15000,
+    length: []
+};
+
+const employeeReducer = (state = {name:"Gun", age:23}, action) => {
+    switch (action.type) {
+        case "setName":
+            state = {
+                ...state,
+                name: action.payload
+            }
+            break;
+        case "setAge":
+            state = {
+                ...state,
+                age: action.payload
+            }
+            break;
+        default:
+    }
+    return state;
+};
+
+const salaryReducer = (state=initialState, action) => {
+    switch (action.type) {
+        case "show":
+            state = {
+                ...state, // space operator
+                salary: state.salary += action.payload,
+                length: [...state.length, action.payload]
+                // salary: state.salary,
+                // length: state.length
+            };
+            break;
+        default:
+    }
+    return state;
+};
+
+const store = createStore(combineReducers({salaryReducer, employeeReducer}));
+
+store.subscribe(() => {
+    console.log("Update Store :", store.getState());
+});
+
+store.dispatch({
+    type: "show",
+    payload: 12000
+});
+
+store.dispatch({
+    type: "show",
+    payload: 13000
+});
+
+store.dispatch({
+    type: "setName",
+    payload: "Kran"
+});
+
+store.dispatch({
+    type: "setAge",
+    payload: 18
+});
